d19cbc19718b61e82283da10a1542ae3c7c950bb
“okbartz” <bartk1612@gmail.com>
2023-11-08 15:56:26 +0000

    Removing Typo file + Adding Validation

---
+++ Dec2Hex.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+//Bartlomiej Klich
+
+class Dec2Hex
+{
+
+    public static int Arg1;
+
+    public static void main(String args[])
+    {
+	try {
+            if (args[0].isEmpty() == false){
+            Arg1 = Integer.parseInt(args[0]);
+            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            int rem, num;
+            num = Arg1;
+            String hexadecimal = "";
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+            while (num != 0) {
+                rem = num % 16;
+                hexadecimal = ch[rem] + hexadecimal;
+                num = num / 16;
+            }
+
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
+        }
+            else {
+                System.out.println("there is no input provided");
+
+            }
+        }
+        catch(Exception e) {
+            System.out.println("please use a integer input from 1 to 16");
+        }
+    }
+}
+
