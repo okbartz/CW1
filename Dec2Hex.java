48fc76a37f0b6ef9271ca4ea78d93845bfc791f0
“okbartz” <bartk1612@gmail.com>
2023-11-11 16:31:09 +0000

    code optimization

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 //Bartlomiej Klich
 
 class Dec2Hex
 {
-
     public static int Arg1;
 
     public static String main(String[] args)
     {
+        Logger logger
+                = Logger.getLogger(
+                Dec2Hex.class.getName());
+
         try {
             if (args[0].isEmpty() == false){
                 Arg1 = Integer.parseInt(args[0]);
-                char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-                int rem, num;
+                char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+                int rem;
+                int num;
                 num = Arg1;
                 String hexadecimal = "";
                 System.out.println("Converting the Decimal Value " + num + " to Hex...");
@@ -24,16 +30,16 @@ class Dec2Hex
                     num = num / 16;
                 }
 
-                System.out.println("Hexadecimal representation is: " + hexadecimal);
+                logger.log(Level.INFO,"Hexadecimal representation is: " + hexadecimal);
                 return hexadecimal;
             }
             else {
-                System.out.println("there is no input provided");
+                logger.log(Level.INFO,"there is no input provided");
                 return "there is no input provided";
             }
         }
         catch(Exception e) {
-            System.out.println("please use a positive integer input");
+            logger.log(Level.INFO,"please use a positive integer input");
             return "please use a positive integer input";
         }
     }
