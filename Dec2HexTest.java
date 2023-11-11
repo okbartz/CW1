067880256caf93e5ea2b6edf9a0a9968d99259b3
“okbartz” <bartk1612@gmail.com>
2023-11-11 17:27:54 +0000

    More opti

--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -7,36 +7,31 @@ public class Dec2HexTest {
 
     private Dec2Hex dec2Hex = new Dec2Hex();
 
-    @Before
-    public void setUp() throws Exception {
 
-
-
-    }
 
     @Test
     public void main() {
 
-        assertEquals("Testing for empty value","there is no input provided", dec2Hex.main(new String[]{""}));
+        assertEquals("Testing for empty value","there is no input provided", Dec2Hex.main(new String[]{""}));
 
     }
 
     @Test
     public void normalNumberTest() {
 
-        assertEquals("Testing for number value","A", dec2Hex.main(new String[]{"10"}));
+        assertEquals("Testing for number value","A", Dec2Hex.main(new String[]{"10"}));
 
     }
     @Test
     public void letterTest() {
 
-        assertEquals("Testing for letter value","please use a positive integer input", dec2Hex.main(new String[]{"A"}));
+        assertEquals("Testing for letter value","please use a positive integer input", Dec2Hex.main(new String[]{"A"}));
 
     }
     @Test
     public void negativeNumberTest() {
 
-        assertEquals("Testing for negative number value","please use a positive integer input", dec2Hex.main(new String[]{"-1"}));
+        assertEquals("Testing for negative number value","please use a positive integer input", Dec2Hex.main(new String[]{"-1"}));
 
     }
 
