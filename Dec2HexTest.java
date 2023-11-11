80befef87c428e2c0270c21143f4180b210e04c4
“okbartz” <bartk1612@gmail.com>
2023-11-11 17:12:27 +0000

    Dec2hextest optimization

--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -3,40 +3,40 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Dec2HexTest {
-	
 
-    Dec2Hex dec2hex;
+
+    private Dec2Hex dec2Hex = new Dec2Hex();
 
     @Before
     public void setUp() throws Exception {
 
-        dec2hex = new Dec2Hex();
+
 
     }
 
     @Test
     public void main() {
 
-        assertEquals("Testing for empty value","there is no input provided", dec2hex.main(new String[]{""}));
+        assertEquals("Testing for empty value","there is no input provided", dec2Hex.main(new String[]{""}));
 
     }
 
     @Test
-    public void NormalNumberTest() {
+    public void normalNumberTest() {
 
-        assertEquals("Testing for number value","A", dec2hex.main(new String[]{"10"}));
-
-    }
-    @Test
-    public void LetterTest() {
-
-        assertEquals("Testing for letter value","please use a positive integer input", dec2hex.main(new String[]{"A"}));
+        assertEquals("Testing for number value","A", dec2Hex.main(new String[]{"10"}));
 
     }
     @Test
-    public void NegativeNumberTest() {
+    public void letterTest() {
 
-        assertEquals("Testing for negative number value","please use a positive integer input", dec2hex.main(new String[]{"-1"}));
+        assertEquals("Testing for letter value","please use a positive integer input", dec2Hex.main(new String[]{"A"}));
+
+    }
+    @Test
+    public void negativeNumberTest() {
+
+        assertEquals("Testing for negative number value","please use a positive integer input", dec2Hex.main(new String[]{"-1"}));
 
     }
 
